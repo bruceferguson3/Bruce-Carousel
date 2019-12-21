b1f41b5261041f74e16e1a4e17bcc2ac9b93407f
BruceFergusonIII <bruce.ferguson3@gmail.com>
2019-12-21 12:09:13 -0600

    Add comments explaining what happens in each routes for readability.

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ app.get("/styles", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/styles.css"));
 });
 
+
+// POSTGRES ROUTES
+// Routes included below are get, post, put, and delete routes
+// ******************************************************************************************
 // Retrieves an item from the postgres database
 app.get('/p_getOneProduct', (req, res) => {
     return pg.getOneProduct(Math.floor(Math.random() * 10000000))
@@ -26,7 +30,40 @@ app.get('/p_getOneProduct', (req, res) => {
             res.send(error);
         })
 });
-
+// Creates an item in the postgres database
+app.post('/p_createItem', (req, res) => {
+    return pg.createItem()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+});
+// Updates an item from the postgres database
+app.put('/p_updateProduct', (req, res) => {
+    return pg.updateProduct(Math.floor(Math.random() * 10000000))
+        .then(result => {
+            res.send(result);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+});
+// Deletes an item from the postgres database
+app.delete('/p_deleteProduct', (req, res) => {
+    return pg.deleteOneProduct(Math.floor(Math.random() * 10000000))
+        .then(result => {
+            res.send(result);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+});
+// ******************************************************************************************
+// MONGO ROUTES
+// Routes included below are get, post, put, and delete routes
+// ******************************************************************************************
 // Retrieves an item from the mongo database
 app.get('/m_getOneProduct', (req, res) => {
     return mongo.m_getOneProduct(1)
@@ -39,18 +76,6 @@ app.get('/m_getOneProduct', (req, res) => {
         })
 
 });
-
-// Creates an item in the postgres database
-app.post('/p_createItem', (req, res) => {
-    return pg.createItem()
-        .then(result => {
-            res.send(result);
-        })
-        .catch(error => {
-            res.send(error);
-        })
-});
-
 // Creates an item in the mongo database
 app.post('/m_createItem', (req, res) => {
     return mongo.m_insertOneProduct()
@@ -61,18 +86,6 @@ app.post('/m_createItem', (req, res) => {
             res.send(err)
         })
 });
-
-// Updates an item from the postgres database
-app.put('/p_updateProduct', (req, res) => {
-    return pg.updateProduct(Math.floor(Math.random() * 10000000))
-        .then(result => {
-            res.send(result);
-        })
-        .catch(error => {
-            res.send(error);
-        })
-});
-
 // Updates an item from the mongo database
 app.put('/m_updateProduct', (req, res) => {
     return mongo.m_updateProduct(1)
@@ -83,18 +96,6 @@ app.put('/m_updateProduct', (req, res) => {
             res.send(error);
         })
 });
-
-// Deletes an item from the postgres database
-app.delete('/p_deleteProduct', (req, res) => {
-    return pg.deleteOneProduct(Math.floor(Math.random() * 10000000))
-        .then(result => {
-            res.send(result);
-        })
-        .catch(error => {
-            res.send(error);
-        })
-});
-
 // Deletes an item from the mongo database
 app.delete('/m_deleteProduct', (req, res) => {
     return mongo.m_deleteOneProduct(3)
@@ -105,8 +106,6 @@ app.delete('/m_deleteProduct', (req, res) => {
             res.send(error);
         })
 });
-
-
 //retrieves all data from the database to render
 app.post("/p_data", (req, res) => {
     return pg.getData()
@@ -117,8 +116,8 @@ app.post("/p_data", (req, res) => {
         res.send(error);
     })
 });
-
-
+// ******************************************************************************************
+// ******************************************************************************************
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
